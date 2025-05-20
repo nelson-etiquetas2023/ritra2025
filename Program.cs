e5d621d9720854b202b551e230b94210e0674c7f
Nelson Pino <devsoftware.etiquetas@gmail.com>
2025-05-20 13:16:44 -0400

    Commit para realizar los cambios hechos en la planta de ritrama

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.Extensions.Configuration;
+
+namespace Ritrama2025
+{
+    internal static class Program
+    {
+        public static IConfiguration? Configuration;
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+
+
+            // To customize application configuration such as set high DPI settings or default font,  
+            // see https://aka.ms/applicationconfiguration.  
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Main());
+        }
+    }
+}
